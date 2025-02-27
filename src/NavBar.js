dde891c583f55d2a3d8b7dc537ad51c1dbf68bdf
wingkym <wingkym@nate.com>
2025-02-27 11:08:38 +0900

    t132132313

--- src/NavBar.js
+++ src/NavBar.js
@@ -5,13 +5,13 @@ const NavBar = () => (
     <nav>
         <ul>
             <li>
-                <Link to="/main">1Home</Link>
+                <Link to="/main">1Home1</Link>
             </li>
             <li>
-                <Link to="/about">2About</Link>
+                <Link to="/about">2About2</Link>
             </li>
             <li>
-                <Link to="/articles">3Articles</Link>
+                <Link to="/articles">3Articles3</Link>
             </li>
         </ul>
     </nav>
